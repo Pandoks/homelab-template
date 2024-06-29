1b7adde7a80da5e2efb916c544a0a97ce8357704
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-06-29 13:34:14 -0700

    Added validating bearer tokens

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,19 @@ import { error, json, text, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
 const luciaAuth: Handle = async ({ event, resolve }) => {
+	const nakedPath = nakedPaths.includes(event.url.pathname);
+	if (nakedPath) {
+		const authorizationHeader = event.request.headers.get('Authorization');
+		const sessionId = lucia.readBearerToken(authorizationHeader ?? '');
+		if (!sessionId) {
+			return new Response(null, { status: 401 });
+		}
+		const { session, user } = await lucia.validateSession(sessionId);
+		event.locals.user = user;
+		event.locals.session = session;
+		return resolve(event);
+	}
+
 	const sessionId = event.cookies.get(lucia.sessionCookieName);
 	if (!sessionId) {
 		event.locals.user = null;
@@ -34,34 +47,41 @@ const luciaAuth: Handle = async ({ event, resolve }) => {
  * CSRF protection copied from sveltekit but with the ability to turn it off for specific routes.
  * Checks if requests are coming from the same origin as the server (basically cors)
  */
-const csrf =
-	(allowedPaths: string[]): Handle =>
-	async ({ event, resolve }) => {
-		const requestType = event.request.headers.get('content-type')?.split(';', 1)[0].trim() ?? '';
-		const forbiddenRequestTypes = ['application/x-www-form-url-urlencoded', 'multipart/form-data'];
+const csrf: Handle = async ({ event, resolve }) => {
+	const requestType = event.request.headers.get('content-type')?.split(';', 1)[0].trim() ?? '';
+	const forbiddenRequestTypes = ['application/x-www-form-url-urlencoded', 'multipart/form-data'];
 
-		const forbidden =
-			event.request.method === 'POST' &&
-			event.request.headers.get('origin') !== event.url.origin &&
-			forbiddenRequestTypes.includes(requestType) &&
-			!allowedPaths.includes(event.url.pathname);
+	const forbidden =
+		event.request.method === 'POST' &&
+		event.request.headers.get('origin') !== event.url.origin &&
+		forbiddenRequestTypes.includes(requestType) &&
+		!nakedPaths.includes(event.url.pathname);
 
-		if (forbidden) {
-			const csrfError = error(
-				403,
-				`Cross-site ${event.request.method} form submissions are forbidden`
-			);
-			if (event.request.headers.get('accept') === 'application/json') {
-				// @ts-ignore
-				return json(csrfError.body, { status: csrfError.status });
-			}
+	if (forbidden) {
+		const csrfError = error(
+			403,
+			`Cross-site ${event.request.method} form submissions are forbidden`
+		);
+		if (event.request.headers.get('accept') === 'application/json') {
 			// @ts-ignore
-			return text(csrfError.body.message, { status: csrfError.status });
+			return json(csrfError.body, { status: csrfError.status });
 		}
+		// @ts-ignore
+		return text(csrfError.body.message, { status: csrfError.status });
+	}
 
-		return resolve(event);
-	};
+	return resolve(event);
+};
 
+/** Paths that don't have CSRF protection
+ *  When making requests to these paths, store the sessiond ID in localstorage and send it over
+ *  as a bearer token:
+ *  fetch('https://api/example.com', {
+ *    headers: {
+ *      Authorization: `Bearer ${sessionId}`
+ *    }
+ *  })
+ */
 const nakedPaths: string[] = [];
 
-export const handle: Handle = sequence(luciaAuth, csrf(nakedPaths));
+export const handle: Handle = sequence(csrf, luciaAuth);
