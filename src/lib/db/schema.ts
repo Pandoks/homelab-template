ee76be47c280b6a405713b7d6f7e4133e3cbcd5f
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-06-29 11:45:10 -0700

    Added schemas for database table

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,5 +1,9 @@
 import { pgTable, uuid } from 'drizzle-orm/pg-core';
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 
 export const users = pgTable('users', {
 	id: uuid('id').primaryKey()
 });
+
+export const insertUserSchema = createInsertSchema(users);
+export const selectUserSchema = createSelectSchema(users);
