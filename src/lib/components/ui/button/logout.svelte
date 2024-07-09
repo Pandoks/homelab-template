d0974fa8ef6378c6e8307f161e7d2bb163af1b71
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-07-09 09:10:10 -0700

    [Logout] added button component

---
+++ src/lib/components/ui/button/logout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Button, type Props } from './index';
+
+  type $$Props = Props & { response: Response };
+
+  let className: $$Props['class'] = undefined;
+  export let variant: $$Props['variant'] = 'default';
+  export let size: $$Props['size'] = 'default';
+  export let builders: $$Props['builders'] = [];
+  export let response: Response; // bind this in parent component
+  export { className as class };
+
+  const handleSignOut = async () => {
+    response = await fetch('/auth/logout', {
+      method: 'post'
+    });
+  };
+</script>
+
+<Button {variant} {size} class={className} {builders} on:click={handleSignOut} {...$$restProps}>
+  <slot />
+</Button>
