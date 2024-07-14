b5b51f5dc42e1d8760de68c76f72fca921ee6710
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-07-13 20:42:55 -0700

    [2FA] animated verify button submission

--- src/routes/auth/2fa/setup/+page.svelte
+++ src/routes/auth/2fa/setup/+page.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
   import { QRCode } from '$lib/components/ui/qr';
-  import { CircleCheck } from '$lib/components/animation/circle-check';
   import type { ActionData, PageData } from './$types';
-  import { fade } from 'svelte/transition';
   import OtpForm from './otp-form.svelte';
-  import { Button } from '$lib/components/ui/button';
 
   export let data: PageData;
   export let form: ActionData;
-
-  let animate = false;
 </script>
 
 <div class="w-full h-screen">
@@ -23,23 +18,12 @@
       <div class="flex gap-4">
         <QRCode class="w-[100px]" value={data.qrCodeLink} />
 
-        {#if animate}
-          <div transition:fade>
-            <p class=" text-muted-foreground text-xs">
-              If you are unable to scan, enter this secret code instead
-            </p>
-            <CircleCheck />
-            <Button><CircleCheck /></Button>
-          </div>
-        {/if}
-        <label>
-          <input type="checkbox" bind:checked={animate} />
-        </label>
+        <p class=" text-muted-foreground text-xs">
+          If you are unable to scan, enter this secret code instead
+        </p>
       </div>
-      <OtpForm data={data.otpForm} success={form ? form.success : false} />
-      {#if form && form.success}
-        <CircleCheck />
-      {/if}
+
+      <OtpForm data={data.otpForm} verified={form ? form.success : false} />
     </div>
   </div>
 </div>
--- src/routes/auth/2fa/setup/otp-form.svelte
+++ src/routes/auth/2fa/setup/otp-form.svelte
@@ -6,15 +6,23 @@
   import { LoaderCircle } from 'lucide-svelte';
   import { zodClient } from 'sveltekit-superforms/adapters';
   import { CircleCheck } from '$lib/components/animation/circle-check';
-  import { Button } from '$lib/components/ui/button';
-  import { draw } from 'svelte/transition';
+  import { fade, fly } from 'svelte/transition';
 
   export let data: SuperValidated<Infer<TwoFactorSetupSchema>>;
-  export let success: boolean = false;
+  export let verified: boolean = false;
+
+  let showCheckmark = false;
+  let showVerify = true;
+
+  $: if (verified) {
+    showCheckmark = true;
+    showVerify = false;
+  }
 
   const superFormFields = superForm(data, {
     validators: zodClient(twoFactorSetupSchema),
-    multipleSubmits: 'prevent'
+    multipleSubmits: 'prevent',
+    invalidateAll: false
   });
 
   const { form: formData, enhance, delayed } = superFormFields;
@@ -23,7 +31,11 @@
 <form class="grid gap-2" method="post" use:enhance action="?/verify-otp">
   <Form.Field form={superFormFields} name="otp">
     <Form.Control let:attrs>
-      <Form.Label>Verification Code</Form.Label>
+      {#if verified}
+        <Form.Label class="text-green-600">Verification Code</Form.Label>
+      {:else}
+        <Form.Label>Verification Code</Form.Label>
+      {/if}
       <div class="flex w-full max-w-sm items-center space-x-3">
         <Input
           {...attrs}
@@ -39,26 +51,35 @@
             Verifying
           </Form.Button>
         {:else}
-          <Form.Button variant="secondary">
-            {#if success}
-              <CircleCheck on:introend={() => console.log('test')} />
-            {:else}
-              Verify
+          <Form.Button variant="secondary" class="w-24 overflow-hidden">
+            {#if showCheckmark}
+              <div out:fly={{ duration: 400, y: 30 }} on:outroend={() => (showVerify = true)}>
+                <CircleCheck
+                  on:introend={() =>
+                    setTimeout(() => {
+                      showCheckmark = false;
+                    }, 2000)}
+                  class="stroke-green-600"
+                />
+              </div>
+            {:else if showVerify}
+              <!-- won't render on first mount. will only animate after click -->
+              <p in:fade={{ duration: 400 }}>Verify</p>
             {/if}
           </Form.Button>
         {/if}
       </div>
     </Form.Control>
   </Form.Field>
-</form>
 
-{#if $delayed}
-  <Form.Button disabled class="w-full mt-4">
-    <LoaderCircle class="mr-2 h-4 w-4 animate-spin" />
-    Activating 2 Factor Authentication
-  </Form.Button>
-{:else if data.valid}
-  <Form.Button class="mt-4">Activate 2 Factor Authentication</Form.Button>
-{:else}
-  <Form.Button disabled class="mt-4">Activate 2 Factor Authentication</Form.Button>
-{/if}
+  {#if $delayed}
+    <Form.Button disabled class="w-full mt-4">
+      <LoaderCircle class="mr-2 h-4 w-4 animate-spin" />
+      Activating 2 Factor Authentication
+    </Form.Button>
+  {:else if data.valid}
+    <Form.Button class="mt-4">Activate 2 Factor Authentication</Form.Button>
+  {:else}
+    <Form.Button disabled class="mt-4">Activate 2 Factor Authentication</Form.Button>
+  {/if}
+</form>
