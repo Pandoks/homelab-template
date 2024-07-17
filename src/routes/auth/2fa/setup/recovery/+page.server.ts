57885100ea5fcaaf2d91acc258d604c5c7767c15
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-07-17 15:18:43 -0700

    Added recovery path

---
+++ src/routes/auth/2fa/setup/recovery/+page.server.ts
@@ -0,0 +1,37 @@
+import { generateIdFromEntropySize, type User } from 'lucia';
+import type { Actions, PageServerLoad } from './$types';
+import { handleLoggedIn } from '$lib/server/auth';
+import { encodeHex } from 'oslo/encoding';
+import { sha256 } from 'oslo/crypto';
+import { db } from '$lib/db';
+import { users } from '$lib/db/schema';
+import { eq } from 'drizzle-orm';
+import { redirect } from '@sveltejs/kit';
+
+export const actions: Actions = {
+  'activate-2fa': async (event) => {
+    const user: User = event.locals.user;
+    await db.update(users).set({ hasTwoFactor: true }).where(eq(users.id, user.id));
+
+    return redirect(302, '/');
+  }
+};
+
+export const load: PageServerLoad = async (event) => {
+  handleLoggedIn(event);
+  const user: User = event.locals.user;
+
+  const twoFactorRecoveryCode = generateIdFromEntropySize(25); // 40 characters
+  const twoFactorRecoveryCodeHash = encodeHex(
+    await sha256(new TextEncoder().encode(twoFactorRecoveryCode))
+  );
+
+  await db
+    .update(users)
+    .set({ twoFactorRecoveryHash: twoFactorRecoveryCodeHash, hasTwoFactor: true })
+    .where(eq(users.id, user.id));
+
+  return {
+    twoFactorRecoveryCode: twoFactorRecoveryCode
+  };
+};
