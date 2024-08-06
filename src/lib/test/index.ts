f4bd012ab59537a23758ac487fcaeefe090c8de8
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-08-06 15:01:24 -0700

    No parameters to reset test db

--- src/lib/test/index.ts
+++ src/lib/test/index.ts
@@ -1,51 +1,46 @@
-import type { RedisInstance } from '$lib/db/redis';
+import { db } from '$lib/db/postgres';
+import { redis, type RedisInstance } from '$lib/db/redis';
 import { sql } from 'drizzle-orm';
 import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import type { RedisClientType, RedisClusterType } from 'redis';
 
-export const resetTestDatabases = async ({
-  dbs,
-  redises
-}: {
-  dbs?: { [id: string]: PostgresJsDatabase };
-  redises?: { [id: string]: RedisInstance };
-}) => {
+export const resetTestDatabases = async () => {
   if (process.env.NODE_ENV !== 'test') {
     throw new Error('You should only be using this in test environments');
   }
 
   let cemetary: Promise<any>[] = [];
 
-  const redisIds = redises ? Object.keys(redises) : [];
-  const dbIds = dbs ? Object.keys(dbs) : [];
+  const redisIds = Object.keys(redis);
+  const dbIds = Object.keys(db);
 
   for (const redisId of redisIds) {
-    const redis = redises![redisId];
-    if (redis.type === 'client') {
-      const redisInstance = redis.instance as RedisClientType;
-      cemetary.push(redisInstance.flushAll());
-    } else if (redis.type === 'cluster') {
-      const redisInstance = redis.instance as RedisClusterType;
-      const masterNodes = redisInstance.masters;
+    const redisInstance = (redis as { [id: string]: RedisInstance })[redisId];
+    if (redisInstance.type === 'client') {
+      const redisClient = redisInstance.instance as RedisClientType;
+      cemetary.push(redisClient.flushAll());
+    } else if (redisInstance.type === 'cluster') {
+      const redisCluster = redisInstance.instance as RedisClusterType;
+      const masterNodes = redisCluster.masters;
       // gotta use a boomer loop because masterNodes' iterator returns strings
       for (let i = 0; i < masterNodes.length; i++) {
         const masterNode = masterNodes[i];
-        const nodeClient = redisInstance.nodeClient(masterNode) as RedisClientType;
+        const nodeClient = redisCluster.nodeClient(masterNode) as RedisClientType;
         cemetary.push(nodeClient.flushAll());
       }
     }
   }
 
   for (const dbId of dbIds) {
-    const db = dbs![dbId];
+    const dbInstance = (db as { [id: string]: PostgresJsDatabase })[dbId];
     const tables: { table_name: string }[] =
-      (await db.execute(sql`
-    SELECT table_name
-    FROM information_schema.tables
-    WHERE table_schema = 'public'
-  `)) || [];
+      (await dbInstance.execute(sql`
+        SELECT table_name
+        FROM information_schema.tables
+        WHERE table_schema = 'public'
+      `)) || [];
     for (const table of tables) {
-      cemetary.push(db!.execute(sql.raw(`TRUNCATE TABLE ${table.table_name} CASCADE`)));
+      cemetary.push(dbInstance!.execute(sql.raw(`TRUNCATE TABLE ${table.table_name} CASCADE`)));
     }
   }
 
