f48f94a08f60c31d315323192b76d42f29832033
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-08-06 13:53:26 -0700

    Reset databases for testing helper function

--- src/lib/test/index.ts
+++ src/lib/test/index.ts
@@ -4,30 +4,46 @@ import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import type { RedisClientType, RedisClusterType } from 'redis';
 
 export const resetTestDatabases = async ({
-  db,
-  redis
+  dbs,
+  redises
 }: {
-  db?: PostgresJsDatabase;
-  redis?: RedisInstance;
+  dbs?: { [id: string]: PostgresJsDatabase };
+  redises?: { [id: string]: RedisInstance };
 }) => {
-  if (!redis) {
-    throw new Error('No test redis instance');
-  } else if (redis.type === 'client') {
-    const redisInstance = redis.instance as RedisClientType;
-    await redisInstance.flushAll();
-  } else {
-    const redisInstance = redis.instance as RedisClusterType;
+  let cemetary: Promise<any>[] = [];
+
+  const redisIds = redises ? Object.keys(redises) : [];
+  const dbIds = dbs ? Object.keys(dbs) : [];
+
+  for (const redisId of redisIds) {
+    const redis = redises![redisId];
+    if (redis.type === 'client') {
+      const redisInstance = redis.instance as RedisClientType;
+      cemetary.push(redisInstance.flushAll());
+    } else if (redis.type === 'cluster') {
+      const redisInstance = redis.instance as RedisClusterType;
+      const masterNodes = redisInstance.masters;
+      // gotta use a boomer loop because masterNodes' iterator returns strings
+      for (let i = 0; i < masterNodes.length; i++) {
+        const masterNode = masterNodes[i];
+        const nodeClient = redisInstance.nodeClient(masterNode) as RedisClientType;
+        cemetary.push(nodeClient.flushAll());
+      }
+    }
   }
 
-  if (!db) {
-    return;
-  }
-  const tables: { table_name: string }[] = await db.execute(sql`
+  for (const dbId of dbIds) {
+    const db = dbs![dbId];
+    const tables: { table_name: string }[] =
+      (await db.execute(sql`
     SELECT table_name
     FROM information_schema.tables
     WHERE table_schema = 'public'
-  `);
-  for (const table of tables) {
-    await db.execute(sql.raw(`TRUNCATE TABLE ${table.table_name} CASCADE`));
+  `)) || [];
+    for (const table of tables) {
+      cemetary.push(db!.execute(sql.raw(`TRUNCATE TABLE ${table.table_name} CASCADE`)));
+    }
   }
+
+  await Promise.all(cemetary);
 };
