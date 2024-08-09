7ffb2ed106f1bd4b09950f72940bef577d73ec89
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-08-08 17:28:46 -0700

    Playwright previews

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   webServer: {
-    command: 'vite preview',
+    command: 'vite preview --mode test',
     port: 4173
   },
   testDir: 'tests',
--- src/lib/db/server/postgres.ts
+++ src/lib/db/server/postgres.ts
@@ -8,6 +8,7 @@ const databases: { [keys: string]: PostgresJsDatabase } = {};
 // add {prepare: false} if using "Transaction" pool mode in Supabase (serverless hosting)
 // otherwise nothing needs to be changed for "Session" (long running hosting)
 if (!building) {
+  console.log(env.USER_DB_DATABASE);
   databases.main = drizzle(
     postgres({
       username: env.USER_DB_USERNAME,
