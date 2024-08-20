a90c8f485736926516ef2d05ec17297ba5afbb45
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-08-20 11:50:07 -0700

    Recover filter test

--- tests/auth/2fa/recover.test.ts
+++ tests/auth/2fa/recover.test.ts
@@ -1,1 +1,30 @@
 import { test } from '../../utils';
+
+test.describe('redirections based off of user conditions', () => {
+  test('should not allow non logged in', async ({ page }) => {
+    const loginRedirect = page.waitForURL('/auth/login');
+    await Promise.all([page.goto('/auth/2fa/recover'), loginRedirect]);
+  });
+
+  test('should not allow already verified', async ({ twoFacPass, twoFacKey }) => {
+    const passHomeRedirect = twoFacPass.page.waitForURL('/');
+    const keyHomeRedirect = twoFacKey.page.waitForURL('/');
+    await Promise.all([
+      twoFacPass.page.goto('/auth/2fa/recover'),
+      twoFacKey.page.goto('/auth/2fa/recover'),
+      passHomeRedirect,
+      keyHomeRedirect
+    ]);
+  });
+
+  test('should not allow non verified emails', async ({ partPass, partKey }) => {
+    const passHomeRedirect = partPass.page.waitForURL('/auth/email-verification');
+    const keyHomeRedirect = partKey.page.waitForURL('/auth/email-verification');
+    await Promise.all([
+      partPass.page.goto('/auth/2fa/recover'),
+      partKey.page.goto('/auth/2fa/recover'),
+      passHomeRedirect,
+      keyHomeRedirect
+    ]);
+  });
+});
