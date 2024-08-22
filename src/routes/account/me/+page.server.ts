1b7c6f20b94dc91d092d8575b85acf4c7f4c9a2a
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-08-21 22:05:39 -0700

    Uncomment

--- src/routes/account/me/+page.server.ts
+++ src/routes/account/me/+page.server.ts
@@ -1,9 +1,9 @@
-// import { protect } from '$lib/auth/server';
-// import type { PageServerLoad } from './$types';
-//
-// export const load: PageServerLoad = async (event) => {
-//   protect({ event: event });
-//
-//   const user = event.locals.user;
-//   const userInfo:
-// };
+import { protect } from '$lib/auth/server';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async (event) => {
+  protect({ event: event });
+
+  const user = event.locals.user;
+  // const userInfo:
+};
