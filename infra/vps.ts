0e1fbdb7038d8b12180758e3b993b0d034037e06
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-08-29 16:49:15 -0700

    Added firewall to vps

--- infra/vps.ts
+++ infra/vps.ts
@@ -1,4 +1,6 @@
 const webVps = new digitalocean.Droplet("Server", {
+  name: "startup-template-vps",
+  tags: ["web"],
   image: "ubuntu-24-04-x64",
   size: "s-1vcpu-512mb-10gb",
   backups: false, // do this manually with s3
@@ -6,12 +8,68 @@ const webVps = new digitalocean.Droplet("Server", {
   gracefulShutdown: true,
   ipv6: true,
   monitoring: true,
-  name: "startup-template-vps",
   region: "sfo3",
   resizeDisk: true,
   sshKeys: ["43190601"],
-  tags: ["web"],
   // userData: initializationScript,
   volumeIds: [], // additional storage
   // vpcUuid: "", // vpc for other services (has as default vpc for itself)
+});
+
+const webFirewall = new digitalocean.Firewall("Firewall", {
+  dropletIds: [webVps.id.apply((str) => parseInt(str, 10))],
+  name: "startup-template-vps-firewall",
+  tags: ["web"],
+  inboundRules: [
+    {
+      // SSH view authentication key
+      protocol: "tcp",
+      portRange: "22",
+      sourceAddresses: ["0.0.0.0/0", "::/0"],
+    },
+    {
+      // HTTPS
+      protocol: "tcp",
+      portRange: "443",
+      sourceAddresses: ["0.0.0.0/0", "::/0"],
+    },
+    {
+      // HTTP (for Let's Encrypt challenges)
+      protocol: "tcp",
+      portRange: "80",
+      sourceAddresses: ["0.0.0.0/0", "::/0"],
+    },
+  ],
+  outboundRules: [
+    {
+      // HTTPS for general secure web traffic
+      protocol: "tcp",
+      portRange: "443",
+      destinationAddresses: ["0.0.0.0/0", "::/0"],
+    },
+    {
+      // HTTP for Let's Encrypt and potential non-HTTPS resources
+      protocol: "tcp",
+      portRange: "80",
+      destinationAddresses: ["0.0.0.0/0", "::/0"],
+    },
+    // {
+    //   // AWS SES SMTP endpoint
+    //   protocol: "tcp",
+    //   portRange: "587",
+    //   destinationAddresses: ["AWS_SES_ENDPOINT_IP"],
+    // },
+    {
+      // DNS queries
+      protocol: "udp",
+      portRange: "53",
+      destinationAddresses: ["0.0.0.0/0", "::/0"],
+    },
+    {
+      // NTP for time synchronization
+      protocol: "udp",
+      portRange: "123",
+      destinationAddresses: ["0.0.0.0/0", "::/0"],
+    },
+  ],
 });
