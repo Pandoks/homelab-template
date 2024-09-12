8f53c4237c87ccfbd810f7aef9f63345b97db6d3
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-09-12 08:50:20 -0700

    Added base name

--- infra/docker.ts
+++ infra/docker.ts
@@ -1,5 +1,7 @@
+import { baseName } from "./utils";
+
 const baseImage = new docker.Image("BaseImage", {
-  imageName: "startup-template-base:latest",
+  imageName: `${baseName}-base:latest`,
   build: {
     context: "../../",
     dockerfile: "../../Dockerfile",
@@ -10,10 +12,13 @@ const baseImage = new docker.Image("BaseImage", {
 const webImage = new docker.Image(
   "WebImage",
   {
-    imageName: "startup-template-web:latest",
+    imageName: `${baseName}-web:latest`,
     build: {
       context: "../../packages/web",
       dockerfile: "../../packages/web/Dockerfile",
+      args: {
+        BASE: baseImage.imageName,
+      },
     },
     skipPush: true,
   },
@@ -21,7 +26,7 @@ const webImage = new docker.Image(
 );
 
 const nginxImage = new docker.Image("NginxImage", {
-  imageName: "startup-template-nginx:latest",
+  imageName: `${baseName}-nginx:latest`,
   build: {
     context: "../../packages/core/src/nginx",
     dockerfile: "../../packages/core/src/nginx/Dockerfile",
@@ -30,7 +35,7 @@ const nginxImage = new docker.Image("NginxImage", {
 });
 
 const mainDatabaseImage = new docker.Image("MainDatabaseImage", {
-  imageName: "startup-template-main-database:latest",
+  imageName: `${baseName}-main-database:latest`,
   build: {
     context: "../../packages/core/src/database/main",
     dockerfile: "../../packages/core/src/database/main/Dockerfile",
@@ -39,10 +44,75 @@ const mainDatabaseImage = new docker.Image("MainDatabaseImage", {
 });
 
 const mainRedisImage = new docker.Image("MainRedisImage", {
-  imageName: "startup-template-main-redis:latest",
+  imageName: `${baseName}-main-redis:latest`,
   build: {
     context: "../../packages/core/src/redis/main",
     dockerfile: "../../packages/core/src/redis/main/Dockerfile",
   },
   skipPush: true,
 });
+
+const dockerNetwork = new docker.Network("DockerNetwork", {
+  name: `${baseName}-docker-network`,
+  driver: "bridge",
+  options: {
+    "com.docker.network.bridge.enable_icc": "true",
+    "com.docker.network.bridge.enable_ip_masquerade": "true",
+  },
+});
+
+const webContainer = new docker.Container(
+  "Web",
+  {
+    name: `${baseName}-web`,
+    image: webImage.imageName,
+    networksAdvanced: [{ name: dockerNetwork.name }],
+    ports: [{ internal: 3000, external: 3000 }],
+  },
+  { dependsOn: [dockerNetwork, webImage], deleteBeforeReplace: true },
+);
+new sst.x.DevCommand(
+  "Web Logs",
+  {
+    dev: { command: $interpolate`docker logs --follow ${webContainer.id}` },
+  },
+  { dependsOn: webContainer },
+);
+
+const nginxContainer = new docker.Container(
+  "Nginx",
+  {
+    name: `${baseName}-nginx`,
+    image: nginxImage.imageName,
+    networksAdvanced: [{ name: dockerNetwork.name }],
+    ports: [
+      // TODO: check if you can connect without exposing ports (cloudflare tunnels)
+      { internal: 80, external: 80 }, // HTTP
+      { internal: 443, external: 443 }, // HTTPS
+    ],
+  },
+  { dependsOn: [dockerNetwork, nginxImage], deleteBeforeReplace: true },
+);
+
+const mainDatabaseContainer = new docker.Container(
+  "MainDatabase",
+  {
+    name: `${baseName}-main-database`,
+    image: mainDatabaseImage.imageName,
+    networksAdvanced: [{ name: dockerNetwork.name }],
+    ports: [{ internal: 5432, external: 5432 }],
+    envs: ["POSTGRES_HOST_AUTH_METHOD=trust"], // Allows no password (NOTE: ONLY FOR DEV)
+  },
+  { dependsOn: [dockerNetwork, mainDatabaseImage], deleteBeforeReplace: true },
+);
+
+const mainRedisContainer = new docker.Container(
+  "MainRedis",
+  {
+    name: `${baseName}-main-redis`,
+    image: mainRedisImage.imageName,
+    networksAdvanced: [{ name: dockerNetwork.name }],
+    ports: [{ internal: 6379, external: 6379 }],
+  },
+  { dependsOn: [dockerNetwork, mainRedisImage], deleteBeforeReplace: true },
+);
--- infra/vps.ts
+++ infra/vps.ts
@@ -1,103 +1,76 @@
-const webVps = new digitalocean.Droplet("Server", {
-  name: "startup-template-vps",
-  tags: ["web"],
-  image: "docker-20-04",
-  size: digitalocean.DropletSlug.DropletS1VCPU1GB,
-  backups: false, // do this manually with s3
-  dropletAgent: false,
-  gracefulShutdown: true,
-  ipv6: true,
-  monitoring: true,
-  region: digitalocean.Region.SFO3,
-  resizeDisk: true,
-  sshKeys: ["43190601"],
-  volumeIds: [], // additional storage
-});
+import { baseName } from "./utils";
 
-const webFirewall = new digitalocean.Firewall("Firewall", {
-  dropletIds: [webVps.id.apply((str) => parseInt(str, 10))],
-  name: "startup-template-vps-firewall",
-  tags: ["web"],
-  inboundRules: [
-    {
-      // SSH
-      protocol: "tcp",
-      portRange: "22",
-      // allow all IP addresses so I can SSH from anywhere
-      sourceAddresses: ["0.0.0.0/0", "::/0"],
-    },
-  ],
-  outboundRules: [
-    {
-      // HTTPS for general secure web traffic
-      protocol: "tcp",
-      portRange: "443",
-      destinationAddresses: ["0.0.0.0/0", "::/0"],
-    },
-    {
-      // HTTP for Let's Encrypt and potential non-HTTPS resources
-      protocol: "tcp",
-      portRange: "80",
-      destinationAddresses: ["0.0.0.0/0", "::/0"],
-    },
-    // {
-    //   // AWS SES SMTP endpoint
-    //   protocol: "tcp",
-    //   portRange: "587",
-    //   destinationAddresses: ["AWS_SES_ENDPOINT_IP"],
-    // },
-    {
-      // DNS queries
-      protocol: "udp",
-      portRange: "53",
-      destinationAddresses: ["0.0.0.0/0", "::/0"],
-    },
-    {
-      // NTP for time synchronization
-      protocol: "udp",
-      portRange: "123",
-      destinationAddresses: ["0.0.0.0/0", "::/0"],
-    },
-  ],
-});
+if (!$dev) {
+  const webVps = new digitalocean.Droplet("Server", {
+    name: `${baseName}-vps`,
+    tags: ["web", $app.stage],
+    image: "docker-20-04",
+    size: digitalocean.DropletSlug.DropletS1VCPU1GB,
+    backups: false, // do this manually with s3
+    dropletAgent: false,
+    gracefulShutdown: true,
+    ipv6: true,
+    monitoring: true,
+    region: digitalocean.Region.SFO3,
+    resizeDisk: true,
+    sshKeys: ["43190601"],
+    volumeIds: [], // additional storage
+  });
 
-/** https://github.com/pulumi/pulumi/discussions/9010 */
-// const dropletDocker = new docker.Provider(
-//   "DropletDocker",
-//   { host: $interpolate`ssh://root@${webVps.ipv4Address}` },
-//   { dependsOn: webVps },
-// );
-//
-// const dockerNetwork = new docker.Network(
-//   "DockerNetwork",
-//   {
-//     name: "startup-template-docker-network",
-//     driver: "bridge",
-//     options: {
-//       "com.docker.network.bridge.enable_icc": "true",
-//       "com.docker.network.bridge.enable_ip_masquerade": "true",
-//     },
-//   },
-//   { provider: dropletDocker },
-// );
+  const webFirewall = new digitalocean.Firewall("Firewall", {
+    dropletIds: [webVps.id.apply((str) => parseInt(str, 10))],
+    name: `${baseName}-firewall`,
+    tags: ["web"],
+    inboundRules: [
+      {
+        // SSH
+        // NOTE: docker image has UFW rate limiting for ssh
+        protocol: "tcp",
+        portRange: "22",
+        // allow all IP addresses so I can SSH from anywhere
+        sourceAddresses: ["0.0.0.0/0", "::/0"],
+      },
+    ],
+    outboundRules: [
+      {
+        // HTTPS for general secure web traffic
+        protocol: "tcp",
+        portRange: "443",
+        destinationAddresses: ["0.0.0.0/0", "::/0"],
+      },
+      {
+        // HTTP for Let's Encrypt and potential non-HTTPS resources
+        protocol: "tcp",
+        portRange: "80",
+        destinationAddresses: ["0.0.0.0/0", "::/0"],
+      },
+      // {
+      //   // AWS SES SMTP endpoint
+      //   protocol: "tcp",
+      //   portRange: "587",
+      //   destinationAddresses: ["AWS_SES_ENDPOINT_IP"],
+      // },
+      {
+        // DNS queries
+        protocol: "udp",
+        portRange: "53",
+        destinationAddresses: ["0.0.0.0/0", "::/0"],
+      },
+      {
+        // NTP for time synchronization
+        protocol: "udp",
+        portRange: "123",
+        destinationAddresses: ["0.0.0.0/0", "::/0"],
+      },
+    ],
+  });
 
-// const nginxContainer = new docker.Container(
-//   "Nginx",
-//   {
-//     name: "startup-template-nginx-container",
-//     image: "startup-template-nginx:latest",
-//     networksAdvanced: [{ name: dockerNetwork.name }],
-//     ports: [
-//       // TODO: check if you can connect without exposing ports
-//       { internal: 80, external: 80 }, // HTTP
-//       { internal: 443, external: 443 }, // HTTPS
-//     ],
-//   },
-//   { provider: dropletDocker },
-// );
-//
-// const mainPostgresContainer = new docker.Container("MainPostgres", {
-//   name: "startup-template-main-postgres-container",
-//   image: "startup-template-main-postgres:latest",
-//   ports: [{ internal: 5432, external: 5432 }],
-// });
+  /** https://github.com/pulumi/pulumi/discussions/9010 */
+  var dropletDocker = new docker.Provider(
+    "DropletDocker",
+    { host: $interpolate`ssh://root@${webVps.ipv4Address}` },
+    { dependsOn: webVps },
+  );
+}
+
+export { dropletDocker };
