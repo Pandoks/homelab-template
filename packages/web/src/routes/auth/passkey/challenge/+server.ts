a41268b424af1decf4cdc73b416dbc1a68bda1c1
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-09-21 03:27:04 +0200

    Added core to passkey challenge

--- packages/web/src/routes/auth/passkey/challenge/+server.ts
+++ packages/web/src/routes/auth/passkey/challenge/+server.ts
@@ -4,8 +4,8 @@ import type { RequestHandler } from './$types';
 import { getRandomValues } from 'crypto';
 import { base64url, encodeHex } from 'oslo/encoding';
 import { sha256 } from 'oslo/crypto';
-import { redis } from '$lib/db/server/redis';
 import { json } from '@sveltejs/kit';
+import { redis as mainRedis } from '@startup-template/core/redis/main/index';
 
 /**
  * Client hits this endpoint to request a challenge and an id for the challenge.
@@ -17,7 +17,7 @@ export const POST: RequestHandler = async (event) => {
   const token = getRandomValues(new Uint8Array(32));
   const tokenHash = encodeHex(await sha256(token));
 
-  await redis.main.instance.set(`passkey-challenge:${id}`, tokenHash, {
+  await mainRedis.set(`passkey-challenge:${id}`, tokenHash, {
     EX: new TimeSpan(5, 'm').seconds()
   });
 
