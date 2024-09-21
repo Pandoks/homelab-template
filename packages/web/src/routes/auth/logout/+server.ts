fae857e769ef28881ab73476777d73300683da33
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-09-21 03:24:51 +0200

    logout uses core

--- packages/web/src/routes/auth/logout/+server.ts
+++ packages/web/src/routes/auth/logout/+server.ts
@@ -1,6 +1,6 @@
 import { error, redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import { lucia } from '$lib/auth/server';
+import { lucia } from '@startup-template/core/auth/server/index';
 
 export const POST: RequestHandler = async (event) => {
   const currentSession = event.locals.session;
