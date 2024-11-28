7bd89c548f275c60d32bfbf4eb715b453b030815
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-11-27 18:32:55 -0800

    Simplify boolean button

--- packages/web/src/routes/auth/2fa/setup/otp-form.svelte
+++ packages/web/src/routes/auth/2fa/setup/otp-form.svelte
@@ -21,45 +21,60 @@
     validators: zodClient(twoFactorSetupSchema),
     multipleSubmits: 'abort',
     resetForm: false,
-    invalidateAll: false,
-    onResult: async (event) => {
-      // await animatedButton.reset();
-      // if (event.result.type === 'success') {
-      //   // needed because animation needs to restart when spamming
-      //   await successElement?.restart();
-      // }
-    }
+    invalidateAll: false
   });
 
   const { form: formData, enhance, delayed } = superFormFields;
 
-  $inspect(form && !form.success);
-  let success = $derived(form && form.success);
-  let fail = $derived(form && !form.success);
-  $inspect('test', fail);
+  let normal = $state(true);
+  let condition = $state(false);
+  let success = $derived(form && form.success && condition);
+  let fail = $derived(form && !form.success && condition);
+  let clickTimeout: NodeJS.Timeout;
 </script>
 
 {#snippet booleanButton()}
-  <Button disabled={$delayed} class="w-24 overflow-hidden" variant="secondary" type="submit">
+  <Button
+    disabled={$delayed}
+    class="w-24 overflow-hidden"
+    variant="secondary"
+    type="submit"
+    onclick={() => {
+      clearTimeout(clickTimeout);
+      clickTimeout = setTimeout(() => {
+        condition = false;
+      }, 1500);
+      condition = true;
+      normal = false;
+    }}
+  >
     {#if $delayed}
       <LoaderCircle class="mr-2 h-4 w-4 animate-spin" />
     {:else if fail}
-      <div in:errorShake out:fly={{ duration: 300, y: 30 }}>
+      <div
+        in:errorShake
+        out:fly={{ duration: 300, y: 30 }}
+        onoutroend={() => {
+          normal = true;
+        }}
+      >
         <CircleX class="stroke-red-600" />
       </div>
     {:else if success}
-      <div out:fly={{ duration: 300, y: 30 }}>
+      <div
+        out:fly={{ duration: 300, y: 30 }}
+        onoutroend={() => {
+          normal = true;
+        }}
+      >
         <CircleCheck class="stroke-green-600" />
       </div>
-    {:else}
+    {:else if normal}
       Verify
     {/if}
   </Button>
 {/snippet}
 
-{#if form && !form.success}
-  <div in:errorShake>test</div>
-{/if}
 <form class="grid gap-2" method="POST" use:enhance action="?/verify-otp">
   <Form.Field form={superFormFields} name="otp">
     <Form.Control>
