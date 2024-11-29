23d90358ebe8e71f9ef7f6c0826815bb0a5d2f48
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-11-28 16:36:07 -0800

    Initial gh actions tests

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from "vitest/config";
-import { config } from "dotenv";
 
 export default defineConfig({
   test: {
+    reporters: process.env.GITHUB_ACTIONS ? ["github-actions"] : "default",
     include: ["./**/*.test.{js,ts}"],
     fileParallelism: false,
     env: {
-      ...config({ path: "../../.env" }).parsed,
+      USER_DB_USERNAME: "user",
+      USER_DB_PASSWORD: "password",
+      USER_DB_HOST: "localhost",
+      USER_DB_PORT: "5432",
+      USER_DB_DATABASE: "userdb",
+      MAIN_REDIS_PASSWORD: "password",
+      MAIN_REDIS_HOST: "localhost",
+      MAIN_REDIS_PORT: "6379",
+      PUBLIC_DOMAIN: "localhost",
+      PUBLIC_APP_NAME: "homelab-template",
     },
   },
   esbuild: {
--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,15 +1,24 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { visualizer } from 'rollup-plugin-visualizer';
-import { config } from 'dotenv';
 
 export default defineConfig({
   plugins: [sveltekit(), visualizer()],
   envDir: '../..',
   test: {
+    reporters: process.env.GITHUB_ACTIONS ? ['github-actions'] : 'default',
     include: ['src/**/*.test.{js,ts}'],
     env: {
-      ...config({ path: '../../.env' }).parsed
+      USER_DB_USERNAME: 'user',
+      USER_DB_PASSWORD: 'password',
+      USER_DB_HOST: 'localhost',
+      USER_DB_PORT: '5432',
+      USER_DB_DATABASE: 'userdb',
+      MAIN_REDIS_PASSWORD: 'password',
+      MAIN_REDIS_HOST: 'localhost',
+      MAIN_REDIS_PORT: '6379',
+      PUBLIC_DOMAIN: 'localhost',
+      PUBLIC_APP_NAME: 'homelab-template'
     },
     fileParallelism: false
   }
