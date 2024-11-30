07582b1fd9096736e87ff9c3535260f170c5f271
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-11-29 18:14:04 -0800

    Added integration test

--- packages/core/src/auth/server/email.test.ts
+++ packages/core/src/auth/server/email.test.ts
@@ -58,7 +58,7 @@ describe("generateEmailVerification", () => {
       .select()
       .from(emailVerifications)
       .where(eq(emailVerifications.email, "test@example.com"));
-    expect(results.length).toBe(0);
+    expect(results.length).toBe(1);
   });
 
   it("should not delete other email verifications", async () => {
@@ -100,6 +100,6 @@ describe("generateEmailVerification", () => {
       .select()
       .from(emailVerifications)
       .where(eq(emailVerifications.email, "test2@example.com"));
-    expect(databaseResultUntouched.code).toBe("WRONGA");
+    expect(databaseResultUntouched.code).toBe("WRONG");
   });
 });
