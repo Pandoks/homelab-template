eb9a9d7794129aa735f1954d972b760ec8d62793
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-12-04 18:16:37 -0800

    redis single dev

--- packages/core/src/redis/main/index.ts
+++ packages/core/src/redis/main/index.ts
@@ -4,6 +4,7 @@ const MAIN_REDIS_PASSWORD = process.env.MAIN_REDIS_PASSWORD;
 const MAIN_REDIS_HOST = process.env.MAIN_REDIS_HOST;
 const MAIN_REDIS_PORT = process.env.MAIN_REDIS_PORT;
 
+console.log("HERE:", MAIN_REDIS_PORT);
 export const redis = createClient({
   password: MAIN_REDIS_PASSWORD,
   socket: {
