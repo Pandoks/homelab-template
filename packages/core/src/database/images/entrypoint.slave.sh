1900d3c55d6b806f978908cbc18690b61b6ce8ca
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-12-10 19:22:41 -0800

    pgbackrest for backup

--- packages/core/src/database/images/entrypoint.slave.sh
+++ packages/core/src/database/images/entrypoint.slave.sh
@@ -4,4 +4,26 @@ pg_basebackup -D /var/lib/postgresql/data/ -h images-masterdb-1 -X stream -c fas
 chmod 700 /var/lib/postgresql/data
 touch /var/lib/postgresql/data/standby.signal
 
-exec $@
+postgres -c config_file=/etc/postgresql/postgresql.conf &
+
+until pg_isready -U $POSTGRES_USER -d $POSTGRES_DB; do
+  sleep 1
+done
+
+PG_PID=$!
+echo "Postgresql is ready"
+
+monitor_pgbackrest() {
+  while true; do
+    wait $PGBR_PID
+    echo "Pgbackrest doesn't exist. Starting pgbackrest..."
+    pgbackrest server --config=/etc/pgbackrest/pgbackrest.conf &
+    PGBR_PID=$!
+    echo "Pgbackrest is ready"
+  done
+}
+monitor_pgbackrest &
+
+# shutdown container when database dies (allows for auto restart recovery)
+wait $PG_PID 
+kill $PGBR_PID
