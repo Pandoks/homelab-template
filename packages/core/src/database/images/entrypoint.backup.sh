b7a274990ba44dd45701b6e892ff904c56f89040
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-12-10 19:09:16 -0800

    Certs

--- packages/core/src/database/images/entrypoint.backup.sh
+++ packages/core/src/database/images/entrypoint.backup.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 setup_stanzas() {
-  until pg_isready -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -p $POSTGRES_PORT; do
+  until pg_isready -h $MASTER_HOST -U $POSTGRES_USER -d $POSTGRES_DB; do
     sleep 1
   done
 
   pgbackrest --stanza=main stanza-create
-  pgbackrest --stanza=main check
-  pgbackrest --stanza=main --type=full backup
 }
 
 setup_stanzas &
