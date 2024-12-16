68f9ebbe76e16cf7f7a40db843a4f36d982a3436
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-12-16 14:08:33 -0800

    Pgbackrest templated

--- packages/core/src/database/images/backup/entrypoint.sh
+++ packages/core/src/database/images/backup/entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-# TODO: LIst of master host process wait
-setup_stanzas() {
-  until pg_isready -h $MASTER_HOSTS -U $POSTGRES_USER -d $POSTGRES_DB; do
-    sleep 1
-  done
-
-  # TODO: Check stanza first and also create stanza from list
-  pgbackrest --stanza=main stanza-create
-  pgbackrest --stanza=main check
-}
-
-setup_stanzas &
+/usr/bin/setup_backups.sh
 
 pgbackrest server --config=/etc/pgbackrest/pgbackrest.conf
