591082099ac8e84d4f60d26ae89db8ceb1964fc9
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-12-16 14:29:57 -0800

    Added pgcat image

--- packages/core/src/database/images/master/entrypoint.sh
+++ packages/core/src/database/images/master/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ $ENV = "development" ]; then
-  /usr/bin/process_env.sh /tmp/conf_templates/postgresql.conf --out /etc/postgresql/postgresql.conf
-  /usr/bin/process_env.sh /tmp/conf_templates/pg_hba.conf --out /etc/postgresql/pg_hba.conf
-  /usr/bin/process_env.sh /tmp/conf_templates/pgbackrest.conf --out /etc/pgbackrest/pgbackrest.conf
+  /usr/local/bin/process_env.sh /tmp/conf_templates/postgresql.conf --out /etc/postgresql/postgresql.conf
+  /usr/local/bin/process_env.sh /tmp/conf_templates/pg_hba.conf --out /etc/postgresql/pg_hba.conf
+  /usr/local/bin/process_env.sh /tmp/conf_templates/pgbackrest.conf --out /etc/pgbackrest/pgbackrest.conf
 else
-  /usr/bin/process_env.sh /etc/postgresql/postgresql.conf
-  /usr/bin/process_env.sh /etc/postgresql/pg_hba.conf
-  /usr/bin/process_env.sh /etc/pgbackrest/pgbackrest.conf
+  /usr/local/bin/process_env.sh /etc/postgresql/postgresql.conf
+  /usr/local/bin/process_env.sh /etc/postgresql/pg_hba.conf
+  /usr/local/bin/process_env.sh /etc/pgbackrest/pgbackrest.conf
 fi
 
 docker-entrypoint.sh postgres -c config_file=/etc/postgresql/postgresql.conf &
