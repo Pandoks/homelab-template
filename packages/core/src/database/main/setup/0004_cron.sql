300e092087dcd161e7d2f0516372587f91a36790
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-12-29 15:27:51 -0800

    Exceptions in sql

--- packages/core/src/database/main/setup/0004_cron.sql
+++ packages/core/src/database/main/setup/0004_cron.sql
@@ -8,6 +8,10 @@ DECLARE
 BEGIN
   DELETE FROM sessions WHERE expires_at < CURRENT_TIMESTAMP RETURNING COUNT(*) INTO deleted_count;
   RETURN deleted_count;
+EXCEPTION
+  WHEN OTHERS THEN
+    RAISE NOTICE 'Error occurred: %', SQLERRM;
+    RETURN 0;
 END;
 $$;
-SELECT cron.schedule('clean-expired-sessions-weekly', '0 2 * * 0', 'SELECT clean_expired_sessions()')
+SELECT cron.schedule('clean-expired-sessions-weekly', '0 2 * * 0', 'SELECT clean_expired_sessions()');
