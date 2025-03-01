21ae85980a6b0b23f44bae168b30e826674cbe9c
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-02-28 21:45:08 -0800

    update: update node version

--- packages/web/src/lib/redis.ts
+++ packages/web/src/lib/redis.ts
@@ -1,9 +1,13 @@
+import { building } from '$app/environment';
 import { env } from '$env/dynamic/private';
 import { createClient, type RedisClientType } from 'redis';
 
-export const mainRedis = (await createClient({ url: env.MAIN_REDIS_URL })
-  .connect()
-  .catch((err) => {
+let mainRedis = createClient({ url: env.MAIN_REDIS_URL });
+if (!building) {
+  mainRedis = (await mainRedis.connect().catch((err) => {
     console.error('Redis Error:');
     console.error(err);
   })) as RedisClientType;
+}
+
+export { mainRedis };
