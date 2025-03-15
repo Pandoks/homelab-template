100f7c8536c2bcf7df848594eb3c0538274634cb
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-03-14 17:52:58 -0700

    wip: setup helm using k3s

--- packages/scripts/dev/setup_k3d.sh
+++ packages/scripts/dev/setup_k3d.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-DOCKER_SUBNET=$(docker network inspect k3d-k3s-default | jq '.[0].IPAM.Config[0].Subnet')
+DOCKER_SUBNET=$(docker network inspect k3d-local-cluster | jq '.[0].IPAM.Config[0].Subnet')
 
 echo $DOCKER_SUBNET
+
+SCRIPT_DIR=$(dirname $(realpath $0))
+PROJECT_ROOT=$SCRIPT_DIR/../../..
+
+
+kubectl apply -f $PROJECT_ROOT/k3s/helm/metallb.yaml
+# TODO wait for metallb to finish
+kubectl apply -f $PROJECT_ROOT/k3s/base/metallb.yaml
+
+kubectl apply -f $PROJECT_ROOT/k3s/helm/haproxy-ingress.yaml
