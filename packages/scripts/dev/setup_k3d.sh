f5605e697dd3685c7e242c7373816e8950f430ae
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-03-20 21:32:23 -0700

    update: wait for all cert-manager deployments

--- packages/scripts/dev/setup_k3d.sh
+++ packages/scripts/dev/setup_k3d.sh
@@ -22,6 +22,8 @@ until kubectl get deployment cert-manager -n cert-manager >/dev/null 2>&1; do
   sleep 1
 done
 kubectl rollout status deployment/cert-manager -n cert-manager --timeout=300s
+kubectl rollout status deployment/cert-manager-webhook -n cert-manager --timeout=300s
+kubectl rollout status deployment/cert-manager-cainjector -n cert-manager --timeout=300s
 kubectl apply -f $PROJECT_ROOT/k3s/base/cert-manager.yaml
 
 kubectl kustomize $PROJECT_ROOT/k3s/dev --load-restrictor=LoadRestrictionsNone | kubectl apply -f -
