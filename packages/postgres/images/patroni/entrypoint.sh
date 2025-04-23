99a34806ffe48cb9d0d79d5cd48d9e792f4f895a
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-04-23 13:41:31 -0700

    wip

--- packages/postgres/images/patroni/entrypoint.sh
+++ packages/postgres/images/patroni/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -e
 
-ls /etc
 # Substitute environment variables in patroni.yaml
 if [ -f /tmp/conf_templates/patroni.yaml ]; then
   envsubst </tmp/conf_templates/patroni.yaml >/etc/patroni.yaml
 fi
 
-# Start Patroni
 exec python3 -m patroni /etc/patroni.yaml
