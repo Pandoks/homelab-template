93d55c1bfad6e22675110566d7a6929e114ff5c6
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-04-28 13:59:47 -0700

    update: pgbackrest started in patroni image

--- packages/postgres/images/patroni/entrypoint.sh
+++ packages/postgres/images/patroni/entrypoint.sh
@@ -3,4 +3,15 @@
 envsubst </tmp/conf_templates/patroni.yaml >/etc/patroni/patroni.yaml
 envsubst </tmp/conf_templates/pgbackrest.conf >/etc/pgbackrest/pgbackrest.conf
 
+monitor_pgbackrest() {
+  while true; do
+    pgbackrest server --config=/etc/pgbackrest/pgbackrest.conf &
+    PGBR_PID=$!
+    echo "Pgbackrest is ready"
+    wait $PGBR_PID
+    echo "Pgbackrest doesn't exist. Restarting pgbackrest..."
+  done
+}
+monitor_pgbackrest &
+
 exec patroni /etc/patroni/patroni.yaml
