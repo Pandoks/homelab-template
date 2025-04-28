ee57eb932d687a84aa3633ef307ef60c71be5d27
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-04-28 09:06:34 -0700

    wip: fix mount

--- packages/postgres/images/patroni/entrypoint.sh
+++ packages/postgres/images/patroni/entrypoint.sh
@@ -3,7 +3,4 @@
 envsubst </tmp/conf_templates/patroni.yaml >/etc/patroni/patroni.yaml
 envsubst </tmp/conf_templates/pgbackrest.conf >/etc/pgbackrest/pgbackrest.conf
 
-chmod 700 /var/lib/postgresql/data
-echo 'chmodded'
-
 exec patroni /etc/patroni/patroni.yaml
