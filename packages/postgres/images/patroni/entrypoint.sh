2abff23b0e59825535b3cf6c5ada988ebf972ac8
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-04-27 17:00:17 -0700

    wip: patroni running in k8s with errors

--- packages/postgres/images/patroni/entrypoint.sh
+++ packages/postgres/images/patroni/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-envsubst </tmp/conf_templates/patroni.yaml >/etc/patroni.yaml
+envsubst </tmp/conf_templates/patroni.yaml >/etc/patroni/patroni.yaml
+envsubst </tmp/conf_templates/pgbackrest.conf >/etc/pgbackrest/pgbackrest.conf
 
-exec patroni /etc/patroni.yaml
+chmod 700 /var/lib/postgresql/data
+echo 'chmodded'
+
+exec patroni /etc/patroni/patroni.yaml
