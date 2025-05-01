ba36eac6b6230cf5af8c20f37b77985ee7934681
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-05-01 11:02:29 -0700

    update: inital working
    needs to connect with s3 and pgbackrest backup server

--- packages/postgres/images/patroni/entrypoint.sh
+++ packages/postgres/images/patroni/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 envsubst </tmp/conf_templates/patroni.yaml >/etc/patroni/patroni.yaml
-envsubst </tmp/conf_templates/pgbackrest.conf >/etc/pgbackrest/pgbackrest.conf
+envsubst </tmp/conf_templates/pgbackrest.conf >/etc/pgbackrest.conf
+
+chmod 700 /var/lib/postgresql/data
 
 monitor_pgbackrest() {
   while true; do
-    pgbackrest server --config=/etc/pgbackrest/pgbackrest.conf &
+    pgbackrest server --config=/etc/pgbackrest.conf &
     PGBR_PID=$!
     echo "Pgbackrest is ready"
     wait $PGBR_PID
