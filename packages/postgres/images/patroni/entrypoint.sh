90ae343f68f24fb77138c1a172809942ea7fb0a1
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-05-06 15:24:06 -0700

    update: supervisord & trixie

--- packages/postgres/images/patroni/entrypoint.sh
+++ packages/postgres/images/patroni/entrypoint.sh
@@ -5,15 +5,4 @@ envsubst </tmp/conf_templates/pgbackrest.conf >/etc/pgbackrest.conf
 
 chmod 700 /var/lib/postgresql/data
 
-monitor_pgbackrest() {
-  while true; do
-    pgbackrest server --config=/etc/pgbackrest.conf &
-    PGBR_PID=$!
-    echo "Pgbackrest is ready"
-    wait $PGBR_PID
-    echo "Pgbackrest doesn't exist. Restarting pgbackrest..."
-  done
-}
-monitor_pgbackrest &
-
-exec $@
+exec supervisord -c /etc/supervisor/supervisord.conf
